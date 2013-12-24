c9bb07b2fd31b209b852dc0b7e4786c59d80aaf7
Elliot Crosby-McCullough <elliot.cm@gmail.com>
2013-12-24 13:14:39 +0000

    Pass feed URL straight through to govukdelivery.
    This greatly simplifies the process of creating
    email signups.
    
    Rather than passing params to a form which sends
    options a controller which then regenerates an
    atom URL by parts, just pass the URL straight
    through and ask the user only for a confirmation
    of their subscription (with local government
    opt-in).

--- app/presenters/publication_filter_json_presenter.rb
+++ app/presenters/publication_filter_json_presenter.rb
@@ -1,7 +1,6 @@
 class PublicationFilterJsonPresenter < DocumentFilterPresenter
   def as_json(options = nil)
-    super.merge atom_feed_url: context.filter_atom_feed_url,
-                email_signup_url: context.filter_email_signup_url(document_type: 'publication_type_all')
+    super.merge atom_feed_url: context.filter_atom_feed_url
   end
 
   def result_type
